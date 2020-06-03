0ced34b3b2b939ddba373453535f1da53fe703f3
Theo Vora <theo.vora@gmail.com>
2020-06-03 15:03:18 -0600

    Add method Position.move which allows input of strings with the format L33 or R6

--- AoC2016-D01/Models/Position.swift
+++ AoC2016-D01/Models/Position.swift
@@ -20,19 +20,52 @@ class Position {
 }
 
 extension Position {
-    func changeDirection(leftOrRight: String) {
-        guard leftOrRight.count == 1 else { return }
+    
+    // MARK: - Main Functions
+    
+    func move(bearing: String) {
+        let bearingTuple = extract(bearing: bearing)
+        let turn = bearingTuple.turn
+        let distance = bearingTuple.distance
         
-        guard leftOrRight == "L" || leftOrRight == "R" else { return }
+        changeDirection(turn)
+        advance(distance)
+    }
+    
+    func advance(_ distance: Int) {
+        let multiplier: Int = self.heading.value
+        let direction = self.heading
         
-        if let turn = Turn(leftOrRight: leftOrRight) {
-            
-            switch turn {
-            case .left:
-                self.heading = self.heading.turnLeft()
-            case .right:
-                self.heading = self.heading.turnRight()
-            }
+        switch direction {
+        case .east, .west:
+            x =  x + (multiplier * distance)
+        case .north, .south:
+            y = y + (multiplier * distance)
         }
+    }
+    
+    func changeDirection(_ turn: Turn) {
+        switch turn {
+        case .left:
+            self.heading = self.heading.turnLeft()
+        case .right:
+            self.heading = self.heading.turnRight()
+        }
+    }
+    
+    
+    // MARK: - Helper functions
+    
+    func extract(bearing: String) -> (turn: Turn, distance: Int) {
+        var turn: Turn = .left
+        let distance = Int(bearing.dropFirst()) ?? 0
+        
+        if bearing.contains("L") {
+            turn = .left
+        } else if bearing.contains("R") {
+            turn = .right
+        }
+        
+        return (turn,distance)
     }
 }
--- AoC2016-D01Tests/AoC2016_D01Tests.swift
+++ AoC2016-D01Tests/AoC2016_D01Tests.swift
@@ -79,13 +79,49 @@ class AoC2016_D01Tests: XCTestCase {
     }
     
     func testChangeDirection() {
-        sut.changeDirection(leftOrRight: "L")
+        sut.changeDirection(.left)
         XCTAssertEqual(sut.heading, .west, "Turning L from North should yield West.")
         
-        sut.changeDirection(leftOrRight: "R")
+        sut.changeDirection(.right)
         XCTAssertEqual(sut.heading, .north, "Turning R from West should yield North.")
     }
     
+    func testExtractBearing() {
+        var testString = "L9"
+        
+        var tuple = sut.extract(bearing: testString)
+        
+        XCTAssertEqual(tuple.turn, .left, "Fed L9 to Position.extract and expected left turn.")
+        
+        XCTAssertEqual(tuple.distance, 9, "Fed L9 to Position.extract and expected distance 9.")
+        
+        testString = "R150"
+        
+        tuple = sut.extract(bearing: testString)
+        
+        XCTAssertEqual(tuple.turn, .right, "Fed R150 to Position.extract and expected right turn.")
+        
+        XCTAssertEqual(tuple.distance, 150, "Fed R150 to Position.extract and expected distance 150.")
+    }
     
+    func testMove() {
+        var bearing = "L33"
+        
+        sut.move(bearing: bearing)
+        
+        XCTAssertEqual(sut.coordinatesAsString, "(-33,0) facing west", "Postion.move was given L33 and expected coordinate to be -33, 0 facing west.")
+        
+        bearing = "L66"
+        
+        sut.move(bearing: bearing)
+        
+        XCTAssertEqual(sut.coordinatesAsString, "(-33,-66) facing south", "Postion.move was given L66 and expected coordinate to be -33, -66 facing south.")
+        
+        bearing = "R3"
+        
+        sut.move(bearing: bearing)
+        
+        XCTAssertEqual(sut.coordinatesAsString, "(-36,-66) facing west", "Postion.move was given L66 and expected coordinate to be -36, -66 facing west.")
+    }
 
 }
